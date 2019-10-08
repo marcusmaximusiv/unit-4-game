febef35a091ca6d4613be17e604224111e070a96
Sterrenberg, Mark <mark.allen.sterrenberg@gmail.com>
2019-10-08 03:21:34 -0500

    Got most of the functionality working as expected. Working on the addition piece

--- assets/audio/Zelda Gems game_files/game.js
+++ assets/audio/Zelda Gems game_files/game.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     var game = {
-        item: ["the Ocarina of Time", "the Master Sword", "a Boomerang", "a Bomb", "a Bow", "an Arrow", "a Recorder", "Food", "a Container Heart"],
+        item: ["Ocarina of Time", "Master Sword", "Boomerang", "Bomb", "Bow", "Arrow", "Recorder", "Food", "Container Heart"],
         score: 0,
         wins: 0,
         losses: 0,
         checkloss: false,
-        checkwin: false,
-        rannum: Math.floor(Math.random() * 121)
+        checkwin: false
     }
     /*
     The lines of code below are used to activate the play and pause button at the top that plays the zelda theme at the top of the page
@@ -25,45 +24,33 @@ $(document).ready(function () {
     var gemElement = document.createElement("audio");
     gemElement.setAttribute("src", "assets/audio/ruppee.wav");
     $("#red_gem").on("click", function () {
-        gemvalue();
+        gemElement.play();
+        var gem_value = game.score + Math.floor(Math.random() * 13);
+        var total = game.score + gem_value;
+        $("#score_value").html(total); //setattribute for the crystal and use the adder
     });
     $("#blue_gem").on("click", function () {
-        gemvalue();
+        gemElement.play();
+        $("#score_value").html(Math.floor(Math.random() * 13));
     });
     $("#green_gem").on("click", function () {
-        gemvalue();
+        gemElement.play();
+        $("#score").append(Math.floor(Math.random() * 13));
     });
     $("#purple_gem").on("click", function () {
-        gemvalue();
+        gemElement.play();
+        $("#score").append(Math.floor(Math.random() * 13));
     });
     /*
     This line of code generates the random number from for the random section 
     */
-    function gemvalue() {
-        gemElement.play();
-        var score = parseInt(game.score);
-        var gem_value = parseInt(Math.floor(Math.random() * 13));
-        var total = parseInt(score) + parseInt(gem_value);
-        $("#score_value").html(total);
-        if (total === game.rannum) {
-            alert("you win!");
-            game.wins++;
-            $("#wins_value").html(game.wins);
-            reset();
-        }
-        else if (total > game.rannum) {
-            alert("you lose!");
-            game.losses++;
-            $("#loss_value").html(game.losses);
-            reset();
-        }
-    }
-
-    $("#rannum").append(game.rannum);
+    $("#rannum").append(Math.floor(Math.random() * 121))
     /*
     The number is randomizing the arry item and making sure the number of ruppees needed for the item changes 
     */
-    $("#item").append(game.item[Math.floor(Math.random() * game.item.length)]);
+    for (i = 0; i < item.length; i++) {
+        $("#item").append(game.item)
+    }
 
     /*
     appends original values to wins, losses, and score column 
@@ -74,27 +61,17 @@ $(document).ready(function () {
 
 
     function reset() {
-        game.wins = 0;
-        game.losses = 0;
-        game.score = 0;
-        game.rannum = Math.floor(Math.random() * 121);
-        $("#score_value").empty();
-        $("#score_value").append(game.score);
-        $("#wins_value").empty();
-        $("#wins_value").append(game.wins);
-        $("#loss_value").empty();
-        $("#loss_value").append(game.losses);
-        $("#rannum").empty();
-        $("#rannum").append(game.rannum);
-        $("#item").empty();
-        $("#item").append(game.item[Math.floor(Math.random() * game.item.length)]);
-        audioElement.pause();
+        wins;
     }
 
     $("#reset").on("click", function () {
-        reset();
+        reset()
     });
 
+    /*if(game.score == game.win){
+        game.
+
+    } */
 
 
 
---
+++ readme.md
@@ -0,0 +1,50 @@
+## Pseudocode of Solution 
+
+Theme 
+ - Make Link theme 
+ - Make background color Link related and scalable 
+
+HTML
+ - Create HTML index.html (CHECK)
+ - do reset.css
+ - do style.css
+
+ Jquery
+ - make random number generator function 1-12 for crystals (check)
+     -function 
+ - make wins function and 
+     -if, else statement 
+ - make lose function
+     -if else statement 
+ - make random number generator function for score (check)
+ - restart game 
+     -function
+ - calculating score 
+ - reset score to zero 
+ - reset total (check)
+    
+ - array for name of sword or object people are buying (1) (check)
+
+
+Completed:
+-reset 
+-gems and them having value
+-creating html 
+-creating adding reset.html 
+-random number generator for score 
+-Getting the array to generate random numbers for the items listed
+-making the array of objects show a random item everytime a reset is done
+-check wins and losses and make sure it is winning or losing 
+
+Challenges:
+
+-Getting addition to work and work as expected
+
+
+
+Notes:
+
+Variable items: Ocarina of Time, Master Sword, Boomerang, Bomb, Bow, Arrow, Recorder, Food, Container Heart, 
+
+
+
